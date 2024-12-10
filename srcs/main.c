ccf7f9615b25c43069b1b12ac652c4899bb72911
Oren Freoua <orfreoua@users.noreply.github.com>
2024-12-10 01:54:08 +0100

    add cross event

--- srcs/main.c
+++ srcs/main.c
@@ -1,14 +1,22 @@
 #include "headers.h"
 
+int	close_window(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	//mlx_destroy_image(data->mlx, data->img.img);
+	free(data->mlx);
+	exit(0);
+}
+
 void	game_loop(t_data *data)
 {
-	/*mlx_loop_hook(data->mlx, &raycasting, data);
-	mlx_hook(data->win, 2, 1L << 0, &press_key, data);
-	mlx_hook(data->win, 3, 1L << 1, &release_key, data);
+	//mlx_loop_hook(data->mlx, &raycasting, data);
+	//mlx_hook(data->win, 2, 1L << 0, &press_key, data);
+	//mlx_hook(data->win, 3, 1L << 1, &release_key, data);
 	if (!OS)
 		mlx_hook(data->win, 33, 1L << 5, close_window, data);
 	else
-		mlx_hook(data->win, 17, 0, close_window, data);*/
+		mlx_hook(data->win, 17, 0, close_window, data);
 	mlx_loop(data->mlx);
 }
 
